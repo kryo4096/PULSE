428d22bfbdb7b33eb3ae445af22758149dab8af3
kryo4096 <kryo4096@gmail.com>
2017-01-11 14:03:41 +0100

    Added functionality

--- src/pulse/Pulse.java
+++ src/pulse/Pulse.java
@@ -1,11 +1,34 @@
 package pulse;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+
 /**
  * Created by kryo4096 on 10.01.2017.
  */
 public class Pulse implements IChatLogger {
-    // TODO
+    public static final int PORT = 11235;
+    private Receiver receiver;
+    private Transmitter transmitter;
     public Pulse(){
-        
+        receiver = new Receiver(PORT,this);
+        transmitter = new Transmitter(PORT,this);
+        receiver.start();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            InetAddress ip = Inet4Address.getByName(br.readLine());
+            while(true){
+                transmitter.sendMessage(ip,br.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
